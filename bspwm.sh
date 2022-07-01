4fafd5881a5a5b5c19bd0e91a13c8d2339b1c3d4
RexDoisPontoZero <orexbest@gmail.com>
2022-07-01 13:05:43 -0300

    added mkdir for some folders

--- bspwm.sh
+++ bspwm.sh
@@ -4,12 +4,14 @@ git clone https://aur.archlinux.org/yay-bin.git
 cd yay-bin && makepkg -si
 yay -S alacritty exa bspwm sxhkd feh polybar xorg-xsetroot xorg-xbacklight light pamixer picom-jonaburg-git dunst rofi flameshot ksuperkey nerd-fonts-jetbrains-mono polkit-gnome fm6000 network-manager-applet helix xfce4-power-manager betterlockscreen zsh zsh-autosuggestions zsh-syntax-highlighting oh-my-zsh-git catppuccin-gtk-theme tela-icon-theme-purple-git --needed
 cd bspwm-dotfiles
-cp -R .config/* ~/.config/
-chmod -R +x ~/.config/bspwm
+mkdir $HOME/.config
+mkdir $HOME/.local/bin
+cp -r .config/* ~/.config/
+chmod -r +x ~/.config/bspwm
 cp .zshrc ~
 cp .zshrc-personal ~
 mkdir ~/.local/bin
-cp -R .local/bin/* ~/.local/bin
+cp -r .local/bin/* ~/.local/bin
 chmod -R +x ~/.local/bin
 betterlockscreen -u ~/.config/bspwm/backgrounds/wave.png
 sudo systemctl enable betterlockscreen@$USER.service

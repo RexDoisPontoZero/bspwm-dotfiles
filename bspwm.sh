aed09f2e4ea7d2be0a0e80e20086a4b08a8a8856
RexDoisPontoZero <orexbest@gmail.com>
2022-10-09 18:06:04 -0300

    Changed base theme color from purple to red.

--- bspwm.sh
+++ bspwm.sh
@@ -2,10 +2,11 @@
 sudo pacman -Syu --needed git base-devel
 git clone https://aur.archlinux.org/yay-bin.git
 cd yay-bin && makepkg -si
-yay -S alacritty zscroll exa bspwm sxhkd feh polybar xorg-xsetroot xorg-xbacklight light pamixer picom-jonaburg-git dunst rofi flameshot ksuperkey nerd-fonts-jetbrains-mono polkit-gnome fm6000 network-manager-applet helix xfce4-power-manager betterlockscreen zsh zsh-autosuggestions zsh-syntax-highlighting oh-my-zsh-git catppuccin-gtk-theme tela-icon-theme-purple-git --needed
+yay -S alacritty xcursor-simp1e-catpuccin zscroll-git micro fish exa bspwm sxhkd feh polybar xorg-xsetroot xorg-xbacklight light pamixer picom-jonaburg-git dunst rofi flameshot ksuperkey nerd-fonts-jetbrains-mono polkit-gnome fm6000 network-manager-applet helix xfce4-power-manager betterlockscreen zsh zsh-autosuggestions zsh-syntax-highlighting oh-my-zsh-git catppuccin-gtk-theme-mocha tela-icon-theme-git --needed
 git clone https://github.com/RexDoisPontoZero/bspwm-dotfiles.git --depth 1
 cd bspwm-dotfiles
 mkdir $HOME/.config
+mkdir $HOME/.local
 mkdir $HOME/.local/bin
 cp -r .config/* ~/.config/
 chmod -r +x ~/.config/bspwm
